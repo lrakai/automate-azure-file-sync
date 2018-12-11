011f3a4a36f481b837a5ec9b34ae5b5ccbae7376
Logan Rakai <lmrakai@gmail.com>
2018-12-11 00:36:41 -0700

    Remove unnecessary get cmdlet

--- Remove-Lab.ps1
+++ Remove-Lab.ps1
@@ -2,7 +2,6 @@
 $ResourceGroupScope = Get-AzureRmResourceGroup -Name $Lab
 Remove-AzureRmPolicyAssignment -Name $PolicyAssignmentName -Scope $ResourceGroupScope.ResourceId
 Remove-AzureRmPolicyDefinition -Name $PolicyDefinitionName -Force
-Get-AzureRmRoleAssignment -RoleDefinitionName $RoleDefinitionName -Scope $ResourceGroupScope.ResourceId
 Remove-AzureRmRoleAssignment -SignInName $User -ResourceGroupName $Lab -RoleDefinitionName $RoleDefinitionName
 Remove-AzureRmRoleDefinition -Name $RoleDefinitionName -Scope $ResourceGroupScope.ResourceId -Force
 Remove-AzureADUser -ObjectId $User
