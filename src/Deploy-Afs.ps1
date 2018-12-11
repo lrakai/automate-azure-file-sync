0b8cecac6c3e7bd8602ec5ac01bdc0de132b37ac
Logan Rakai <lmrakai@gmail.com>
2018-12-11 15:40:52 -0700

    Register Server with Storage Sync Service

---
+++ src/Deploy-Afs.ps1
@@ -0,0 +1,61 @@
+function Login-Azure {
+    param (
+        [string]$username,
+        [string]$password # Use [secureString] outside of secure environments
+    )
+
+
+    # Login non-interactively
+    $acctInfo = Login-AzureRmAccount
+    return $acctInfo
+}
+
+function New-StorageSyncService {
+    param (
+        $acctInfo,
+        [string]$storageSyncName,
+        [string]$resourceGroupName
+    )
+    # The location of the Azure File Sync Agent
+    $agentPath = "C:\Program Files\Azure\StorageSyncAgent"
+
+    # Import the Azure File Sync management cmdlets 
+    # (cmdlets not yet included in the Azure PowerShell Module)
+    Import-Module "$agentPath\StorageSync.Management.PowerShell.Cmdlets.dll"
+
+    # Store your subscription and Azure Active Directory tenant ID 
+    $subID = $acctInfo.Context.Subscription.Id
+    $tenantID = $acctInfo.Context.Tenant.Id
+
+    # Get the resource group to determine the location of the sync service
+    $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName
+    $location = $resourceGroup.Location
+
+    # The following command creates an AFS context 
+    # it enables subsequent AFS cmdlets to be executed with minimal 
+    # repetition of parameters or separate authentication 
+    Login-AzureRmStorageSync `
+        –SubscriptionId $subID `
+        -ResourceGroupName $resourceGroupName `
+        -TenantId $tenantID `
+        -Location $location
+
+    # Create a new Storage Sync Service in the
+    # Login-AzureRmStorageSync context
+    New-AzureRmStorageSyncService -StorageSyncServiceName $storageSyncName
+}
+
+function Register-ServerEndpoint {
+    param (
+        [string]$storageSyncName
+    )
+    New-AzureRmStorageSyncService -StorageSyncServiceName $storageSyncName
+}
+
+
+$resourceGroupName = "cal-###"
+$storageSyncName = "sync"
+
+$acctInfo = Login-Azure
+New-StorageSyncService $acctInfo $storageSyncName $resourceGroupName
+Register-ServerEndpoint $storageSyncName
