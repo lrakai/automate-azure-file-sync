cf87a07857f70ed88d01f3ef5c78bd24b23e0a16
Logan Rakai <lmrakai@gmail.com>
2018-12-12 01:09:40 -0700

    Fix Out-File cmdlet

--- src/Test-Afs.ps1
+++ src/Test-Afs.ps1
@@ -1,6 +1,6 @@
 function Test-Afs {
     # Write text to a file in the server endpoint path (D:\dev)
-    "Sync me" | OutFile D:\dev\test.txt
+    "Sync me" | Out-File D:\dev\test.txt
 }
 
 Test-Afs
